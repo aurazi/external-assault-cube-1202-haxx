bf2043f092beaccbf9632e7e779e2c0f1a07af99
aura <69578093+aurazi@users.noreply.github.com>
2022-03-27 09:58:07 +0100

    now you can modify health and ammo

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use std::ffi::{CStr, CString};
+use std::io;
 use std::mem;
+use std::str::FromStr;
+use winapi::ctypes::c_void;
 use winapi::shared::{
     minwindef::{DWORD, FALSE, HMODULE},
     ntdef::{CHAR, NULL},
@@ -7,6 +10,7 @@ use winapi::shared::{
 use winapi::um::{
     errhandlingapi::GetLastError,
     handleapi::CloseHandle,
+    memoryapi::{ReadProcessMemory, WriteProcessMemory},
     processthreadsapi::OpenProcess,
     psapi::EnumProcessModules,
     psapi::GetModuleFileNameExA,
@@ -16,6 +20,7 @@ use winapi::um::{
     winnt::PROCESS_ALL_ACCESS,
     winuser::{FindWindowA, GetWindowThreadProcessId},
 };
+// External Simple Hack for Assault Cube ver 1202
 
 fn main() {
     unsafe {
@@ -98,11 +103,63 @@ fn main() {
                 );
             }
             let base_address = module_info.lpBaseOfDll;
-            let entry_point_address = module_info.EntryPoint;
-            println!(
-                "Base Address: {}\nEntry Point Address: {}",
-                base_address as usize, entry_point_address as usize
+            println!("Base Address: {:#08x}\n", base_address as usize,);
+            /*
+            Local Entity Object Offset: + 10F4F4   (static)
+                Health                : + F8   (4 bytes)
+                Default Weapon Ammo   : + 150  (4 bytes)
+            */
+            let mut health = 0u32;
+            let mut ammo = 0u32;
+
+            let mut address_entity_object_addr: usize = 0;
+            ReadProcessMemory(
+                process_handle,
+                (base_address as usize + 0x10F4F4) as *mut c_void,
+                &mut address_entity_object_addr as *mut _ as *mut c_void,
+                mem::size_of::<usize>(),
+                core::ptr::null_mut(),
             );
+            let health_pointer = (address_entity_object_addr + 0xF8) as *mut c_void;
+            let ammo_pointer = (address_entity_object_addr + 0x150) as *mut c_void;
+            ReadProcessMemory(
+                process_handle,
+                health_pointer,
+                &mut health as *mut _ as *mut c_void,
+                mem::size_of::<u32>(),
+                core::ptr::null_mut(),
+            );
+            ReadProcessMemory(
+                process_handle,
+                ammo_pointer,
+                &mut ammo as *mut _ as *mut c_void,
+                mem::size_of::<u32>(),
+                core::ptr::null_mut(),
+            );
+            println!("Current Health: {}", health);
+            println!("Current Ammo  : {}", ammo);
+
+            println!("Input your desired health: ");
+            let desired_health = get_number_input::<u32>();
+            println!("Input your desired ammo: ");
+            let desired_ammo = get_number_input::<u32>();
+
+            WriteProcessMemory(
+                process_handle,
+                health_pointer,
+                &desired_health as *const _ as *const c_void,
+                mem::size_of::<u32>(),
+                core::ptr::null_mut(),
+            );
+            WriteProcessMemory(
+                process_handle,
+                ammo_pointer,
+                &desired_ammo as *const _ as *const c_void,
+                mem::size_of::<u32>(),
+                core::ptr::null_mut(),
+            );
+
+            println!("Set desired changes!");
         } else {
             println!("Failed to find the module");
         }
@@ -110,5 +167,16 @@ fn main() {
         if CloseHandle(process_handle) == 0 {
             panic!("Failed to close handle?!");
         }
+    }
+}
+fn get_number_input<T: FromStr>() -> T {
+    let mut string = String::new();
+    loop {
+        io::stdin().read_line(&mut string).unwrap();
+        match string.trim().parse() {
+            Ok(number) => return number,
+            Err(_) => (),
+        }
+        string.clear();
     }
 }
